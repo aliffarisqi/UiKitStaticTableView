417547a51ce1890e4d14811bc434db50f3f3adbf
Bayu Alif Farisqi <jakartabanyu@gmail.com>
2024-01-21 06:36:16 +0700

    starter project Static table view in UiKit

---
+++ UiKitStaticTableView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UiKitStaticTableView
+//
+//  Created by Bayu Alif Farisqi on 21/01/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
